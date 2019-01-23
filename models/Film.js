4dc61ad4633ac7c7b7e550e7ae79426c418a1fa5
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-01-23 15:52:27 +0100

    Created model file with Schema
    Now has Schema of movies/film

---
+++ models/Film.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// 1. Create a schema for a film
+let filmSchema = new Schema(  {
+    "title" :  String,
+    "productionCountries" : String,
+    "productionYear" : Number,
+    "length" : Number,
+    "desc" : String,
+    "regi" : String,
+    "actor" : String,
+    "language" : String,
+    "subtitle" : String,
+    "premier" : String,
+    "trailer" : String,
+    "image" : String,
+    "review": String
+  });
+  
+/* Example properties:
+  "author": { type: String, required: true },
+  "link": { type: String, unique: true, required: true },
+*/
+
+// 2. Create a class with methods/or getters/setters
+//    that every books should have
+
+  class filmClass {
+
+    get age(){
+      let currentYear = new Date().getFullYear();
+      return currentYear - this.year;
+    }
+  
+  }
+  
+  // 3. Create the model and export it
+  filmSchema.loadClass(filmClass);
+  module.exports = db.model('Film', filmSchema);
+  
