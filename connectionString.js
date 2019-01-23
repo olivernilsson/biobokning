9e61e305364eabf00623adbfd1ac68b766c788bd
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-01-23 19:39:41 +0100

    correct db login

--- connectionString.js
+++ connectionString.js
@@ -1,2 +1,2 @@
-let connectionString = `mongodb+srv://olivernilsson:nintendo64@cinema-2o7xd.mongodb.net/test?retryWrites=true`;
+let connectionString = `mongodb+srv://olinil:nintendo64.@cinema-2o7xd.mongodb.net/test?retryWrites=true`;
 module.exports = connectionString;
