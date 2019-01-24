3677a05b803f91297ccaa1ca5927cbf8ca72ab91
Delshad <delshadk1@gmail.com>
2019-01-24 18:03:56 +0100

    regform
    regform

--- www/js/RegPage.js
+++ www/js/RegPage.js
@@ -5,22 +5,25 @@ class RegPage extends Component {
     this.addRoute('/regPage', 'regPage');
     this.addEvents({
       'keyup #password': 'validatePasswordInput',
+      'keyup #password-verify': 'validatePasswordInput',
+      'keyup #email': 'emailVerification',
     })
   }
 
 
   validatePasswordInput() {
     let done = 0;
-    let searchValue = $('#password').val();
+    let passwordValue = $('#password').val();
+    let verifyValue = $('#password-verify').val();
     let passwordInput = $('#password').val().length;
-    let bigLetter = /[A-Z]+/.test(searchValue);
-    let oneNumber = /[0-9]+/.test(searchValue);
+    let bigLetter = /[A-Z]+/.test(passwordValue);
+    let oneNumber = /[0-9]+/.test(passwordValue);
 
     if (passwordInput > 7) {
       $('.verify-tecken').css("color", "lightgreen")
       done++;
     } else {
-      $('.verify-tecken').css("color", "white")
+      $('.verify-tecken').css("color", "lightcoral")
       done--;
     }
 
@@ -28,7 +31,7 @@ class RegPage extends Component {
       $('.verify-bokstav').css("color", "lightgreen")
       done++;
     } else {
-      $('.verify-bokstav').css("color", "white")
+      $('.verify-bokstav').css("color", "lightcoral")
       done--;
     }
 
@@ -36,16 +39,47 @@ class RegPage extends Component {
       $('.verify-number').css("color", "lightgreen")
       done++;
     } else {
-      $('.verify-number').css("color", "white")
+      $('.verify-number').css("color", "lightcoral")
       done--;
     }
 
+    if (passwordValue === verifyValue) {
+      done++;
+    } else {
+      done--;
+    }
 
-    if(done===3){
-     $('.done-btn').removeClass('disabled');
-    }else{
+    if (done === 4) {
+      $('.done-btn').removeClass('disabled');
+    } else {
       $('.done-btn').addClass('disabled');
     }
   }
 
+  emailVerification(){
+    let emailInput = $('#email').val();
+    let emailLength = $('#email').val().length;
+    let afterDot = emailInput.substring(emailInput.lastIndexOf("."));
+
+    if(emailLength<9){
+      
+    }
+
+    if(emailInput.indexOf('@') > 3){
+      console.log('done')
+    }
+
+
+    if(emailInput.substring("@")>2){
+    console.log('yey')
+    }
+
+ 
+
+
+  }
+
 }
+
+
+
