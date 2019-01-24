8fc51cae56515f553be6bf2a02d4649f178750bc
Delshad <delshadk1@gmail.com>
2019-01-24 22:49:08 +0100

    regform validation
    regform validation

--- www/js/RegPage.js
+++ www/js/RegPage.js
@@ -4,82 +4,122 @@ class RegPage extends Component {
     super();
     this.addRoute('/regPage', 'regPage');
     this.addEvents({
-      'keyup #password': 'validatePasswordInput',
-      'keyup #password-verify': 'validatePasswordInput',
+      'keyup #firstname': 'validateRegForm',
+      'keyup #lastname': 'validateRegForm',
+      'keyup #password': 'validateRegForm',
+      'keyup #password-verify': 'validateRegForm',
       'keyup #email': 'emailVerification',
     })
+
+
+    this.done = false;
+    this.emailValid = false;
   }
 
 
-  validatePasswordInput() {
-    let done = 0;
+  validateRegForm() {
+    let firstName = $('#firstname').val();
+    let lastName = $('#lastname').val();
     let passwordValue = $('#password').val();
     let verifyValue = $('#password-verify').val();
     let passwordInput = $('#password').val().length;
     let bigLetter = /[A-Z]+/.test(passwordValue);
     let oneNumber = /[0-9]+/.test(passwordValue);
+    let styleCount = 0;
 
     if (passwordInput > 7) {
       $('.verify-tecken').css("color", "lightgreen")
-      done++;
+      styleCount++;
     } else {
       $('.verify-tecken').css("color", "lightcoral")
-      done--;
+      styleCount--;
     }
 
     if (bigLetter === true) {
       $('.verify-bokstav').css("color", "lightgreen")
-      done++;
+      styleCount++;
     } else {
       $('.verify-bokstav').css("color", "lightcoral")
-      done--;
+      styleCount--;
     }
 
     if (oneNumber === true) {
       $('.verify-number').css("color", "lightgreen")
-      done++;
+      styleCount++;
     } else {
       $('.verify-number').css("color", "lightcoral")
-      done--;
+      styleCount--;
     }
 
-    if (passwordValue === verifyValue) {
-      done++;
+    if (styleCount === 3) {
+      $(`#password`).css("border-bottom", "1px solid lightgreen")
     } else {
-      done--;
+      $(`#password`).css("border-bottom", "1px solid lightcoral")
     }
 
-    if (done === 4) {
+    if (passwordValue === verifyValue && passwordInput>7) {
+
+      $(`#password-verify`).css("border-bottom", "1px solid lightgreen")
+    } else {
+      $(`#password-verify`).css("border-bottom", "1px solid lightcoral")
+    }
+
+
+
+    if (firstName.length > 2) {
+      $(`#firstname`).css("border-bottom", "1px solid lightgreen")
+    } else {
+      $(`#firstname`).css("border-bottom", "1px solid lightcoral")
+    }
+
+
+    if (lastName.length > 2) {
+      $(`#lastname`).css("border-bottom", "1px solid lightgreen")
+    } else {
+      $(`#lastname`).css("border-bottom", "1px solid lightcoral")
+    }
+
+
+    if (firstName.length > 2 && lastName.length > 2 && passwordValue === verifyValue && passwordInput > 7) {
+
+      this.done = true;
+    } else {
+      this.done = false;
+    }
+
+
+    if (this.done === true && this.emailValid === true) {
       $('.done-btn').removeClass('disabled');
     } else {
       $('.done-btn').addClass('disabled');
     }
   }
 
-  emailVerification(){
+  emailVerification() {
+    let minLength = false;
     let emailInput = $('#email').val();
     let emailLength = $('#email').val().length;
-    let afterDot = emailInput.substring(emailInput.lastIndexOf("."));
+    let dot = '.';
+    let indexOfAtSign = emailInput.indexOf('@');
+    let indexOfLastDot = emailInput.lastIndexOf(dot);
 
-    if(emailLength<9){
-      
+
+
+    if (emailLength > 9) {
+      minLength = true;
     }
 
-    if(emailInput.indexOf('@') > 3){
-      console.log('done')
+    if (minLength === true && indexOfAtSign > 3 && indexOfLastDot > 3) {
+      $('.verify-email').css("color", "lightgreen")
+      $('#email').css("border-bottom", "1px solid lightgreen")
+      this.emailValid = true;
+
+    } else {
+      $('.verify-email').css("color", "lightcoral")
+      $('#email').css("border-bottom", "1px solid lightcoral")
     }
-
-
-    if(emailInput.substring("@")>2){
-    console.log('yey')
-    }
-
- 
-
 
   }
 
 }
-
-
 
