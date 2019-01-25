5bc01c23c96e72f2c4241ff2e9aa2c0a14d1aff8
Delshad <delshadk1@gmail.com>
2019-01-25 11:13:25 +0100

    random viw

---
+++ www/js/RandomView.js
@@ -0,0 +1,24 @@
+class RandomView extends Component {
+  constructor() {
+    super();
+    this.addRoute('/', 'Start');
+    this.movies = ['test1', 'test2', 'test3', 'test4']
+    this.salon = ['salon1', 'salon2', 'salon3', 'salon4']
+    this.startPage = this.startPage;
+
+    this.randomize();
+  }
+
+
+
+
+
+  randomize() {
+
+
+    console.log('okey')
+
+
+
+  }
+}
