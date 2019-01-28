fd0bfa08fa54af7c33c8ca15bb94376b7aa792c8
Delshad <delshadk1@gmail.com>
2019-01-28 12:11:18 +0100

    model fix
    Ändrade lite i modelen, tog bort lite text i ticket

--- models/Ticket.js
+++ models/Ticket.js
@@ -4,10 +4,7 @@ const Schema = mongoose.Schema;
 let ticketSchema = new Schema ( {
     "adult": Number,
     "children": Number,
-    "pensionary": Number,
-    "family2 + 1": Number,
-    "family 2 + 2": Number,
-    "family big": Number,
+    "pensionary": Number
 });
 //class in case its needed
 class TicketClass {
