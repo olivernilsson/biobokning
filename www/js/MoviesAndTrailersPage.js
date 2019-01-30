561c74919dbdd95026ba6225a1027bdd01d985bb
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-01-30 14:09:52 +0100

    updated method name and title for dropdown

--- www/js/MoviesAndTrailersPage.js
+++ www/js/MoviesAndTrailersPage.js
@@ -3,11 +3,11 @@ class MoviesAndTrailersPage extends Component {
   constructor(){
     super();
     this.addRoute('/moviesandtrailers', 'Movies and Trailers');
-    this.testCheckLogin();
+    this.moviePrint();
     this.movieArray = [];
   }
 
-  async testCheckLogin(){
+  async moviePrint(){
     //console.log(await Film.find());
     this.movieArray = await Film.find();
     for(let i in this.movieArray){
