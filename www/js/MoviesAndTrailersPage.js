8bc11951217eb848738542d61fcdfd1df6fdc3aa
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-01-30 13:17:29 +0100

    added print out for movies

--- www/js/MoviesAndTrailersPage.js
+++ www/js/MoviesAndTrailersPage.js
@@ -3,5 +3,16 @@ class MoviesAndTrailersPage extends Component {
   constructor(){
     super();
     this.addRoute('/moviesandtrailers', 'Movies and Trailers');
+    this.testCheckLogin();
+    this.movieArray = [];
+  }
+
+  async testCheckLogin(){
+    //console.log(await Film.find());
+    this.movieArray = await Film.find();
+    for(let i in this.movieArray){
+    console.log(this.movieArray[i].title);
+    }
+
   }
 }
