15068a684144602ab35a48bcc986577c1904c0f1
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-02-04 10:15:39 +0100

    movie list without append

--- www/js/MoviesAndTrailersPage.js
+++ www/js/MoviesAndTrailersPage.js
@@ -11,11 +11,13 @@ class MoviesAndTrailersPage extends Component {
   //incorrect, dont use
   async moviePrint(){
     //console.log(await Film.find());
-    this.movieArray = await Film.find();
-    for(let i in this.movieArray){
-    //console.log(this.movieArray[i].title);
-    $(".dropdown-menu").append(`<a class="dropdown-item">${this.movieArray[i].title}</a>`);
+    let movies = await Film.find();
+    console.log(movies);
+    for(let movie of movies){
+    this.movieArray.push(movie.title);
     }
+    console.log(this.movieArray);
+    this.render();
 
   }
 }
