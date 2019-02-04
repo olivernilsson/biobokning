d25fa2bebe101d44f18d1dbc3e5805286ad38a7f
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-02-04 12:04:17 +0100

    prints movie info when clicked

--- www/js/MoviesAndTrailersPage.js
+++ www/js/MoviesAndTrailersPage.js
@@ -3,21 +3,26 @@ class MoviesAndTrailersPage extends Component {
   constructor(){
     super();
     this.addRoute('/moviesandtrailers', 'Movies and Trailers');
-    this.movieArray = [];
+    this.movies = [];
     this.moviePrint();
+    this.addEvents({
+      'click .dropdown-item': 'movieSelect'
+    })
   }
 
 
-  //incorrect, dont use
   async moviePrint(){
-    //console.log(await Film.find());
-    let movies = await Film.find();
-    console.log(movies);
-    for(let movie of movies){
-    this.movieArray.push(movie.title);
-    }
-    console.log(this.movieArray);
+    this.movies = await Film.find();
+    console.log(this.movies);
     this.render();
 
+  }
+
+  movieSelect(e){
+    let id = $(e.currentTarget).attr('data-movie-id');
+    let movie = this.movies.filter(movie => movie._id === id)[0];
+    console.log(movie);
+    this.movie = movie;
+    this.render();
   }
 }
