31cd92072b9c3d6af825e2ad09732edf20c0e629
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-02-07 10:39:13 +0100

    erased "sök filmer"
    we will probably not use this..

--- www/js/NavBar.js
+++ www/js/NavBar.js
@@ -6,7 +6,6 @@ class NavBar extends Component {
       new NavItem('Start', '/'),
       new NavItem('Om oss', '/about'),
       new NavItem('Filmer & trailers', '/moviesandtrailers'),
-      new NavItem('Sök filmer', '/searchmovie'),
       new NavItem('BookDemo','/bookdemo'),
       new UserRegistration('')
     ];
