9e8ec8e83bf3f91e413176b835a801dbb6d6a08f
sunsneezer <xil_wong@hotmail.com>
2019-02-12 22:02:49 +0100

    Expected merged conflicts but none detected - gitkraken, I trusted you...

--- www/js/AboutPage.js
+++ www/js/AboutPage.js
@@ -2,11 +2,27 @@ class AboutPage extends Component {
 
   constructor(){
     super();
+    this.addRoute('/about', 'About');
+    this.addEvents({
+      'click .btn-details': 'toggleDetails',
+      'click .btn-error': 'triggerError'
+    });
+    this.showDetails = false;
+  }
 
-    
+  unmount(){
+    this.showDetails = false;
+    this.showExampleOfError = false;
+  }
 
-  } 
+  triggerError(){
+    this.showExampleOfError = true;
+    this.render();
+  }
 
-  
+  toggleDetails(){
+    this.showDetails = !this.showDetails;
+    this.render();
+  }
 
 }
