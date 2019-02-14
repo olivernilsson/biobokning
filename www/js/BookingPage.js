40e3eefa74e9f6cc3fc5275c0dd000fa24f31dff
Delshad <delshadk1@gmail.com>
2019-02-14 11:26:01 +0100

    fix

--- www/js/BookingPage.js
+++ www/js/BookingPage.js
@@ -18,63 +18,71 @@ class BookingPage extends Component {
     this.salonPage = new Salon();
     this.pricePage = new PricePage();
     this.bookingConfirm = new BookingConfirm();
-    this.userLogin =  new UserLogin();
-  }
-
+    this.userLogin = new UserLogin();
     this.totalPersons;
-    this.bookedSeats=[];
-
+    this.bookedSeats = [];
   }
 
-  change(selectedView){
-    //console.log(selectedView)
-    this.view= selectedView;
+
+
+
+  change(selectedView) {
+    console.log(selectedView)
+    this.view = selectedView;
     this.render()
   }
-  async toggleRegPage(){
-    if(!((await Login.find()).error)){
+  async toggleRegPage() {
+    if (!((await Login.find()).error)) {
       this.regPage = new Button();
     } else {
       this.regPage = new RegPage();
     }
     this.render();
   }
-  async mount(){
+  async mount() {
     let id = this.routeParts[0];
-    let view = await View.find(id);
-   Object.assign(this, view._props);
-  this.checkLogin();
+    this.view = await View.find(id);
+    Object.assign(this, this.view._props);
     this.render();
- 
+
   }
 
   countUp() {
     this.stepCounter++;
     if (this.stepCounter > 4) { this.stepCounter = 4; }
     this.render();
-    this.dataChanges();   
+    this.dataChanges();
+  }
+  countDown() {
+    this.stepCounter--;
+    if (this.stepCounter < 1) {
+      App.moviesAndTrailersPage.changeVal();
+      this.stepCounter = 1
+    }
+    this.render();
+
   }
 
-  async bookTicket() {
-
-  dataChanges(){
-    if(this.stepCounter==2){
-      this.totalPersons= this.pricePage.adults+this.pricePage.kids+this.pricePage.seniors;
+  dataChanges() {
+    if (this.stepCounter == 2) {
+      this.totalPersons = this.pricePage.adults + this.pricePage.kids + this.pricePage.seniors;
       this.salonPage.nbrOfPickedSeats = this.totalPersons;
       console.log(this.salonPage.nbrOfPickedSeats);
     }
-    if(this.stepCounter==3){
+    if (this.stepCounter == 3) {
       this.bookedSeats = this.salonPage.bookedSeats;
       console.log(this.bookedSeats);
     }
 
   }
 
+  async bookTicket() {
+
     let populatedBooking = await Booking.find(`.findOne({bookingId:'hejhej'})
     .populate('view')
     .populate('user')
     .exec()
-    `); 
+    `);
 
     let getTheUser = await User.find(`.findOne({firstName: 'aaa'})`);
 
@@ -84,7 +92,7 @@ class BookingPage extends Component {
       seniors: this.pricePage.seniors,
       user: getTheUser._id,
       seats: this.bookedSeats,
-      view: this.view._id
+      view: this.view
     })
     await myNewBooking.save();
 
@@ -92,55 +100,33 @@ class BookingPage extends Component {
     .populate('view')
     .populate('user')
     .exec()
-    `); 
+    `);
 
     console.log(myNewBookingPopulated);
 
     //---- Nedan skickar vi data till confirm sidan ---//   
-      /*
-        this.bookingConfirm.bookingId = myNewBooking.bookingId;
-        this.bookingConfirm.totalPrice = this.TotalPersons * 100;
-        this.bookingConfirm.seats = myNewBooking.seats;
-        this.bookingConfirm.film = myNewBooking.view.film;
-        this.bookingConfirm.date = myNewBooking.view.date;
-        this.bookingConfirm.time = myNewBooking.view.time;
-        this.bookingConfirm.salon = myNewBooking.view.auditorium;
-
-        this.bookingConfirm.adults = myNewBooking.adults;
-        this.bookingConfirm.kids = myNewBooking.kids;
-        this.bookingConfirm.seniors = myNewBooking.seniors;
-      */
+    /*
+      this.bookingConfirm.bookingId = myNewBooking.bookingId;
+      this.bookingConfirm.totalPrice = this.TotalPersons * 100;
+      this.bookingConfirm.seats = myNewBooking.seats;
+      this.bookingConfirm.film = myNewBooking.view.film;
+      this.bookingConfirm.date = myNewBooking.view.date;
+      this.bookingConfirm.time = myNewBooking.view.time;
+      this.bookingConfirm.salon = myNewBooking.view.auditorium;
+  
+      this.bookingConfirm.adults = myNewBooking.adults;
+      this.bookingConfirm.kids = myNewBooking.kids;
+      this.bookingConfirm.seniors = myNewBooking.seniors;
+    */
 
   }
+
+
+
+
+
+
 
 }
 
 
-
-  change(selectedView){
-    console.log(selectedView)
-    this.view= selectedView;
-this.render()
-  }
-
-  countUp() {
-    this.stepCounter++;
-    if (this.stepCounter > 4) { this.stepCounter = 4; }
-    this.render();
-
-
-  }
-
-  countDown() {
-    this.stepCounter--;
-    if (this.stepCounter < 1 ) { 
-      App.moviesAndTrailersPage.changeVal();
-      this.stepCounter = 1
-   }
-    this.render();
-
-  }
-
-
-
-}
