5ed2592c8aaa32dd0cd7f55aedb481028a9f7816
robinasplund <r.asplund@mail.com>
2019-02-21 11:13:32 +0100

    Current booking done

--- www/js/MyBookings.js
+++ www/js/MyBookings.js
@@ -3,6 +3,7 @@ class MyBookings extends Component {
         super();
         this.addRoute('/mina-bookningar', 'Bokningar')
         //this.bokings = [];
+        this.currentHtmlResult = "";
         this.htmlresult = "";
         this.userBookings = []
         this.getBookingsHistory();
@@ -17,15 +18,7 @@ class MyBookings extends Component {
 
         let getTheUser = await User.find(`.find({email:'${this.email}'})`);
         let userBookings = getTheUser[0].bookings;
-        let getTheBooking = await Booking.find(`.find({_id:'${userBookings[0]}'})`);
 
-
-
-
-        let popuBooking = await Booking.find(`.find({_id:'${userBookings[0]}'})
-        .populate('view')
-        .exec()
-        `);
         let popuUser = await User.find(`.find({email:'${this.email}'})
         .populate({path: 'bookings',
         populate: { path: 'view' }
@@ -33,10 +26,61 @@ class MyBookings extends Component {
         `);
 
 
-        // console.log(popuBooking)
+
+
+        let getCurrentBooking = userBookings.pop();
+
+        let populateCurrent = await Booking.find(`.find({_id:'${getCurrentBooking}'})
+            .populate('view')
+            .exec()
+            `);
+        console.log(populateCurrent[0].bookingId);
+
+        let currentSeats = populateCurrent[0].seats
+        this.currentSeat = []
+
+        for (let seat of currentSeats) {
+            this.currentSeat.push(seat)
+        }
+        this.currentSeatmod = this.currentSeat.join('-');
+
+        this.currentHtmlResult += `
+ 
+        
+           <div class="card">
+           <div class="card-header" id="${populateCurrent[0]._id}">
+ 
+               <button class="btn btn-link booking-button" data-toggle="collapse" data-target="#${populateCurrent[0].bookingId}" aria-expanded="false" aria-controls="${populateCurrent[0].bookingId}">
+                <h4 class="booking-title"> Bokning: ${populateCurrent[0].bookingId} -  Datum : ${populateCurrent[0].view.date}</h4>
+               </button>
+ 
+           </div>
+ 
+           <div id="${populateCurrent[0].bookingId}" class="collapse" aria-labelledby="${populateCurrent[0]._id}" data-parent="#current-booking">
+             <div class="card-body">
+             <p class="booking-film">Film: ${populateCurrent[0].view.film}</p>
+              <p class="booking-salon"> Salong: ${populateCurrent[0].view.auditorium}</p>
+              <p class="booking-time">Tid: ${populateCurrent[0].view.time}</p>
+            <p class="booking-seat-style"> Platserna: ${this.currentSeatmod}</p>
+             </div>
+           </div>
+         </div>
+ 
+           `;
+
+
+
+
+
+
+
+
+
+
+
 
         let bookings = popuUser[0].bookings
-
+        bookings.pop();
         for (let view of bookings) {
 
             let seats = view.seats
@@ -48,18 +92,19 @@ class MyBookings extends Component {
             }
             this.seatmod = this.seat.join('-');
 
+
             this.htmlresult += `
 
            <div class="card">
            <div class="card-header" id="${view._id}">
              
-               <button class="btn btn-link booking-button" data-toggle="collapse" data-target="#${view.bookingId}" aria-expanded="false" aria-controls="${view.bookingId}">
+               <button class="btn btn-link booking-button" data-toggle="collapse" data-target="#B${view.bookingId}" aria-expanded="false" aria-controls="B${view.bookingId}">
                 <h4 class="booking-title"> Bokning: ${view.bookingId} -  Datum : ${view.view.date}</h4>
                </button>
 
            </div>
        
-           <div id="${view.bookingId}" class="collapse" aria-labelledby="${view._id}" data-parent="#my-bookings">
+           <div id="B${view.bookingId}" class="collapse" aria-labelledby="${view._id}" data-parent="#my-bookings">
              <div class="card-body">
              <p class="booking-film">Film: ${view.view.film}</p>
               <p class="booking-salon"> Salong: ${view.view.auditorium}</p>
