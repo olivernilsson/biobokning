6f8fcba04690eacf6a68c6244dac25dab9e655f8
Delshad <delshadk1@gmail.com>
2019-02-27 14:25:39 +0100

    fixes

--- www/js/MyBookings.js
+++ www/js/MyBookings.js
@@ -23,9 +23,6 @@ class MyBookings extends Component {
     this.loggedIn = await User.find(`.find(
        {email: '${this.email}'})`);
 
-    let getTheUser = await User.find(`.find({email:'${this.email}'})`);
-    let userBookings = getTheUser[0].bookings;
-
     let popuUser = await User.find(`.find({email:'${this.email}'})
         .populate({path: 'bookings',
         populate: { path: 'view' }
@@ -42,86 +39,88 @@ class MyBookings extends Component {
         this.activeBooking.push(view);
         console.log(this.activeBooking);
       } else {
-        this.historyBooking.push(view.view);
-        console.log(view.view);
+        this.historyBooking.push(view);
+        console.log(view);
       }
     }
 
-    // let getCurrentBooking = userBookings.pop();
+    for (let view of this.activeBooking) {
+      let seats = view.seats;
+      this.currentSeat = [];
 
-    // let populateCurrent = await Booking.find(`.find({_id:'${getCurrentBooking}'})
-    //     .populate('view')
-    //     .exec()
-    //     `);
-    // //console.log(populateCurrent[0].bookingId);
+      for (let seat of seats) {
+        this.currentSeat.push(seat);
+      }
 
-    // let currentSeats = populateCurrent[0].seats
-    // this.currentSeat = []
+      this.currentSeatmod = this.currentSeat.join("-");
 
-    // for (let seat of currentSeats) {
-    //     this.currentSeat.push(seat)
-    // }
-    // this.currentSeatmod = this.currentSeat.join('-');
+      this.currentHtmlResult += `
 
-    // this.currentHtmlResult += `
+      <div class="card">
+      <div class="card-header" id="${view._id}">
 
-    //    <div class="card">
-    //    <div class="card-header" id="${populateCurrent[0]._id}">
+          <button class="btn btn-link booking-button" data-toggle="collapse" data-target="#B${
+            view.bookingId
+          }" aria-expanded="false" aria-controls="B${view.bookingId}">
+           <h4 class="booking-title"> Bokning: ${view.bookingId} -  Datum : ${
+        view.view.date
+      }</h4>
+          </button>
 
-    //        <button class="btn btn-link booking-button" data-toggle="collapse" data-target="#G${populateCurrent[0].bookingId}" aria-expanded="false" aria-controls="G${populateCurrent[0].bookingId}">
-    //         <h4 class="booking-title"> Bokning: ${populateCurrent[0].bookingId} -  Datum : ${populateCurrent[0].view.date}</h4>
-    //        </button>
+      </div>
 
-    //    </div>
+      <div id="B${view.bookingId}" class="collapse" aria-labelledby="${
+        view._id
+      }" data-parent="#my-bookings">
+        <div class="card-body">
+        <p class="booking-film">Film: ${view.view.film}</p>
+         <p class="booking-salon"> Salong: ${view.view.auditorium}</p>
+         <p class="booking-time">Tid: ${view.view.time}</p>
+         <p class="booking-seat-style"> Platserna: ${this.currentSeatmod}</p>
+        </div>
+      </div>
+    </div>
 
-    //    <div id="G${populateCurrent[0].bookingId}" class="collapse" aria-labelledby="${populateCurrent[0]._id}" data-parent="#current-booking">
-    //      <div class="card-body">
-    //      <p class="booking-film">Film: ${populateCurrent[0].view.film}</p>
-    //       <p class="booking-salon"> Salong: ${populateCurrent[0].view.auditorium}</p>
-    //       <p class="booking-time">Tid: ${populateCurrent[0].view.time}</p>
-    //     <p class="booking-seat-style"> Platserna: ${this.currentSeatmod}</p>
-    //      </div>
-    //    </div>
-    //  </div>
+      `;
+    }
 
-    //    `;
+    for (let view of this.historyBooking) {
+      let seats = view.seats;
+      this.seat = [];
 
-    // let bookings = popuUser[0].bookings
-    // bookings.pop();
-    // for (let view of bookings) {
+      for (let seat of seats) {
+        this.seat.push(seat);
+      }
+      this.seatmod = this.seat.join("-");
 
-    //     let seats = view.seats
-    //     this.seat = []
+      this.htmlresult += `
 
-    //     for (let seat of seats) {
-    //         this.seat.push(seat)
+       <div class="card">
+       <div class="card-header" id="${view._id}">
 
-    //     }
-    //     this.seatmod = this.seat.join('-');
+           <button class="btn btn-link booking-button" data-toggle="collapse" data-target="#B${
+             view.bookingId
+           }" aria-expanded="false" aria-controls="B${view.bookingId}">
+            <h4 class="booking-title"> Bokning: ${view.bookingId} -  Datum : ${
+        view.view.date
+      }</h4>
+           </button>
 
-    //     this.htmlresult += `
+       </div>
 
-    //    <div class="card">
-    //    <div class="card-header" id="${view._id}">
+       <div id="B${view.bookingId}" class="collapse" aria-labelledby="${
+        view._id
+      }" data-parent="#my-bookings">
+         <div class="card-body">
+         <p class="booking-film">Film: ${view.view.film}</p>
+          <p class="booking-salon"> Salong: ${view.view.auditorium}</p>
+          <p class="booking-time">Tid: ${view.view.time}</p>
+          <p class="booking-seat-style"> Platserna: ${this.seatmod}</p>
+         </div>
+       </div>
+     </div>
 
-    //        <button class="btn btn-link booking-button" data-toggle="collapse" data-target="#B${view.bookingId}" aria-expanded="false" aria-controls="B${view.bookingId}">
-    //         <h4 class="booking-title"> Bokning: ${view.bookingId} -  Datum : ${view.view.date}</h4>
-    //        </button>
-
-    //    </div>
-
-    //    <div id="B${view.bookingId}" class="collapse" aria-labelledby="${view._id}" data-parent="#my-bookings">
-    //      <div class="card-body">
-    //      <p class="booking-film">Film: ${view.view.film}</p>
-    //       <p class="booking-salon"> Salong: ${view.view.auditorium}</p>
-    //       <p class="booking-time">Tid: ${view.view.time}</p>
-    //       <p class="booking-seat-style"> Platserna: ${this.seatmod}</p>
-    //      </div>
-    //    </div>
-    //  </div>
-
-    //    `;
-
-    // }
+       `;
+    }
   }
 }
