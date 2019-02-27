145cd59c29ffbe5b91db8e2e159d11bf4e689ca6
Delshad <delshadk1@gmail.com>
2019-02-27 16:18:13 +0100

    faf

--- models/Booking.js
+++ models/Booking.js
@@ -1,28 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let bookingSchema = new Schema({
-   adults: Number,
-   kids: Number,
-   seniors: Number,
-   user: {type: Schema.Types.ObjectId, ref: 'User'},
-   view: { type: Schema.Types.ObjectId, ref: 'View' },
-   seats: Array,
-   bookingId: { type: String, unique: true }
+  adults: Number,
+  kids: Number,
+  seniors: Number,
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  view: { type: Schema.Types.ObjectId, ref: "View" },
+  seats: Array,
+  bookingId: { type: String, unique: true }
 });
-
 
 function randomItem(arr) {
-   return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-bookingSchema.pre('save', function () {
-   //  randomize a booking id
-   if (this.bookingId) { return; }
-   let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-   let bookingId = '';
-   while (bookingId.length < 6) { bookingId += randomItem(alphabet); }
-   this.bookingId = bookingId;
+bookingSchema.pre("save", function() {
+  //  randomize a booking id
+  if (this.bookingId) {
+    return;
+  }
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let bookingId = "";
+  while (bookingId.length < 6) {
+    bookingId += randomItem(alphabet);
+  }
+  this.bookingId = bookingId;
 });
 
-module.exports = db.model('Booking', bookingSchema);
+bookingSchema.pre("validate", async function(next, done) {
+  let takenSeats = [];
+  let bookingz = await this.constructor.find({ view: this.view });
+
+  for (let booking of bookingz) {
+    takenSeats = takenSeats.concat(booking.seats);
+  }
+
+  for (let seat of this.seats) {
+    if (takenSeats.includes(seat)) {
+      next(new Error("this seat is taken"));
+      return;
+    }
+  }
+  next();
+});
+
+module.exports = db.model("Booking", bookingSchema);
