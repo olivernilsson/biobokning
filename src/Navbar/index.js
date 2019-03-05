3c09a0c973c83d2ee7d6f97fa6150ce5d0dc1f2f
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-03-05 14:17:01 +0100

    React AboutPage Inserted

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -60,6 +60,15 @@ class NavbarApp extends Component {
               <NavItem>
                 <NavLink
                   className="nav-link"
+                  to="/aboutpage"
+                  activeClassName="active"
+                >
+                  AboutPage
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className="nav-link"
                   to="/aboutsalons"
                   activeClassName="active"
                 >
