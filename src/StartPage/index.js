804ac8cc029a5279e8f347bc0bd1b83ab5f92778
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-03-09 12:33:45 +0100

    Printing header image on start page

--- src/StartPage/index.js
+++ src/StartPage/index.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
+import "./startpage.scss";
+import REST from "./REST.js";
+
+class Film extends REST {}
 
 class StartPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { mIndex: 4, movies: [] };
+    this.film();
   }
+
+  async film() {
+    this.movie = await Film.find();
+    this.setState({ movies: this.movie });
+  }
+
   render() {
+    if (this.state.movies.length === 0) {
+      return <div />;
+    }
     return (
-      <div>
-        <p>Iam the startPage WIHI!</p>
-      </div>
+      <section>
+        <a href="/moviesandtrailerspage">
+          <div className="head-movie" data-movie-id="${this.singleMovieId}">
+            <div className="headoverlay">
+              <img
+                className="head-image"
+                src={require("./" +
+                  this.state.movies[this.state.mIndex].images[0])}
+              />
+            </div>
+            <div className="headoverlay-title">Hunter Killer - Biljetter</div>
+          </div>
+        </a>
+      </section>
     );
   }
 }
