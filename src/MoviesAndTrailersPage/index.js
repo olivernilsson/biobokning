0ffa836654ce9f5046ea9c651080e1f1cf934d98
Delshad <delshadk1@gmail.com>
2019-03-12 19:29:35 +0100

    fix

--- src/MoviesAndTrailersPage/index.js
+++ src/MoviesAndTrailersPage/index.js
@@ -15,7 +15,8 @@ class MoviesAndTrailersPage extends Component {
       mIndex: this.props.location.index || 0,
       modal: false,
       movies: [],
-      views: []
+      views: [],
+      selectedMovie: []
     };
     this.youtube = "https://www.youtube.com/embed/";
     this.toggle = this.toggle.bind(this);
@@ -24,34 +25,29 @@ class MoviesAndTrailersPage extends Component {
     this.testlist = [];
     this.viewings = [];
     //this.setMovie();
+  }
+
+  async componentDidMount() {
+    let route = window.location.href.split("/").pop();
+    let movieUrl = route.replace(/\-/g, " ");
+    console.log(movieUrl);
+    this.chosenMovie = await Film.find(`.find({title:"${movieUrl}"})`);
+    this.setState({
+      selectedMovie: this.chosenMovie[0]
+    });
     this.start();
   }
 
-  setMovie() {
-    if (this.props.location.index !== undefined) {
-      //console.log(this.props.location.index);
-      this.setState({ mIndex: this.props.location.index });
-    }
-    //console.log(this.state.mIndex);
-  }
-
   async start() {
-    await this.film();
-    await this.viewingsfind(this.state.movies[this.state.mIndex]);
-  }
-
-  async film() {
-    this.movie = await Film.find();
-    //console.log(this.movie[0].title);
-    this.setState({ movies: this.movie });
+    await this.viewingsfind(this.state.selectedMovie);
   }
 
   async viewingsfind(movie) {
-    //console.log(movie.title);
+    //console.log(movie.title)
+
     this.list = [];
     //console.log(`${movie.title}`);
     this.test = JSON.stringify(movie.title);
-    this.viewings = await View.find(`.find({film:${this.test}})`);
 
     //console.log(this.viewings);
 
@@ -64,10 +60,6 @@ class MoviesAndTrailersPage extends Component {
     this.setState({ views: this.testlist });
   }
 
-  returnFilm() {
-    return this.movie[0].title;
-  }
-
   toggle() {
     this.setState(prevState => ({
       modal: !prevState.modal
@@ -75,26 +67,30 @@ class MoviesAndTrailersPage extends Component {
   }
 
   render() {
-    if (this.state.movies.length === 0) {
+    let movie = this.state.selectedMovie;
+    if (this.state.selectedMovie > 0) {
       return <div />;
     }
 
     return (
+      <section>
+        <h1>hej</h1>
+      </section>
       <section className="movie-section">
         <div className="movie-fade" />
         <img
           className="bg-image"
           alt="bg"
-          src={require("./" + this.state.movies[this.state.mIndex].images[0])}
+          src={require("./" + movie.images[0])}
         />
-        <img
+      //   <img
           className="play"
           alt="play-button"
           onClick={this.toggle}
           src={require("./play.png")}
         />
-        <div>
-          <Modal
+      //   <div>
+      //     <Modal
             isOpen={this.state.modal}
             toggle={this.toggle}
             className={this.props.className}
@@ -110,75 +106,75 @@ class MoviesAndTrailersPage extends Component {
                   this.youtube +
                   this.state.movies[this.state.mIndex].youtubeTrailers[0]
                 }
-              />
-            </ModalBody>
-          </Modal>
-        </div>
-        <div className="page-info">
-          <h2 className="movieh2">
-            {this.state.movies[this.state.mIndex].title}
-          </h2>
-          <h4 className="movieh4">
-            {"År: " +
-              this.state.movies[this.state.mIndex].productionYear +
-              " | Minuter: " +
-              this.state.movies[this.state.mIndex].length +
-              " | Genre: " +
-              this.state.movies[this.state.mIndex].genre}
-          </h4>
-          <br />
-          <p>{this.state.movies[this.state.mIndex].description}</p>
-          <p>{"Regissör: " + this.state.movies[this.state.mIndex].director}</p>
-          <p>
-            {"Skådespelare: " +
-              this.state.movies[this.state.mIndex].actors
-                .map(actor => `${actor}`)
-                .join(", ")}{" "}
-          </p>
-          <p>{"Språk: " + this.state.movies[this.state.mIndex].language}</p>
-          <br />
-        </div>
+      //         />
+      //       </ModalBody>
+      //     </Modal>
+      //   </div>
+      //   <div className="page-info">
+      //     <h2 className="movieh2">
+      //       {this.state.movies[this.state.mIndex].title}
+      //     </h2>
+      //     <h4 className="movieh4">
+      //       {"År: " +
+      //         this.state.movies[this.state.mIndex].productionYear +
+      //         " | Minuter: " +
+      //         this.state.movies[this.state.mIndex].length +
+      //         " | Genre: " +
+      //         this.state.movies[this.state.mIndex].genre}
+      //     </h4>
+      //     <br />
+      //     <p>{this.state.movies[this.state.mIndex].description}</p>
+      //     <p>{"Regissör: " + this.state.movies[this.state.mIndex].director}</p>
+      //     <p>
+      //       {"Skådespelare: " +
+      //         this.state.movies[this.state.mIndex].actors
+      //           .map(actor => `${actor}`)
+      //           .join(", ")}{" "}
+      //     </p>
+      //     <p>{"Språk: " + this.state.movies[this.state.mIndex].language}</p>
+      //     <br />
+      //   </div>
 
-        <h2 className="act-view">Aktuella visningar: </h2>
-        <div className="viewings-list">
-          <div className="row-top">
-            <table className="viewings-table">
-              <tbody>
-                <tr>
-                  <td>Film </td>
-                  <td>Salong </td>
-                  <td>Datum</td>
-                  <td>Tid </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div className="viewings-list">
-          {this.testlist.map(listitem => (
-            <Link
-              className="view-select"
-              to={"/bookingpage/" + listitem._id}
-              data-view-id={listitem._id}
-              key={listitem._id}
-            >
-              <div className="row-view">
-                <table className="viewings-table">
-                  <tbody>
-                    <tr>
-                      <td>{listitem.film} </td>
-                      <td>{listitem.auditorium} </td>
-                      <td>{listitem.date} </td>
-                      <td>{listitem.time} </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </Link>
-          ))}
-        </div>
-        <br />
-      </section>
+      //   <h2 className="act-view">Aktuella visningar: </h2>
+      //   <div className="viewings-list">
+      //     <div className="row-top">
+      //       <table className="viewings-table">
+      //         <tbody>
+      //           <tr>
+      //             <td>Film </td>
+      //             <td>Salong </td>
+      //             <td>Datum</td>
+      //             <td>Tid </td>
+      //           </tr>
+      //         </tbody>
+      //       </table>
+      //     </div>
+      //   </div>
+      //   <div className="viewings-list">
+      //     {this.testlist.map(listitem => (
+      //       <Link
+      //         className="view-select"
+      //         to={"/bookingpage/" + listitem._id}
+      //         data-view-id={listitem._id}
+      //         key={listitem._id}
+      //       >
+      //         <div className="row-view">
+      //           <table className="viewings-table">
+      //             <tbody>
+      //               <tr>
+      //                 <td>{listitem.film} </td>
+      //                 <td>{listitem.auditorium} </td>
+      //                 <td>{listitem.date} </td>
+      //                 <td>{listitem.time} </td>
+      //               </tr>
+      //             </tbody>
+      //           </table>
+      //         </div>
+      //       </Link>
+      //     ))}
+      //   </div>
+      //   <br />
+      // </section>
     );
   }
 }
